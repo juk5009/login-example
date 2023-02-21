e60b683d6faeba28db2d9880d47a6ac4db000d43
Kjuk <juk6340@gmail.com>
2023-02-21 11:08:56 +0900

    layout 완성

--- src/main/java/shop/mtcoding/loginexample/handler/CustomExceptionHandler.java
+++ src/main/java/shop/mtcoding/loginexample/handler/CustomExceptionHandler.java
@@ -1,11 +1,12 @@
 package shop.mtcoding.loginexample.handler;
 
-import org.h2.tools.Script;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import shop.mtcoding.loginexample.handler.ex.CustomException;
+import shop.mtcoding.loginexample.util.Script;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
@@ -13,7 +14,7 @@ public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<?> customException(CustomException e) {
 
-        // return new ResponseEntity<>(Script.back(e.getMessage()), e.getStatus());
+       
         String responseBody = Script.back(e.getMessage());
         return new ResponseEntity<>(responseBody, e.getStatus());
     }
