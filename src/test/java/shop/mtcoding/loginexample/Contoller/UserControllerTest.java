2c04efd7e49c2e0467f948a3d97e52e11dd6c69c
Kjuk <juk6340@gmail.com>
2023-02-21 16:37:15 +0900

    UserControllerTest 완료

--- src/test/java/shop/mtcoding/loginexample/Contoller/UserControllerTest.java
+++ src/test/java/shop/mtcoding/loginexample/Contoller/UserControllerTest.java
@@ -1,6 +1,5 @@
 package shop.mtcoding.loginexample.Contoller;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
